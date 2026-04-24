67d186108a8e6eb5aac3a6cd1c2b480897dfcede
Chakyl <chakyl1127@gmail.com>
2026-04-23 21:36:10 -0400

    Light level

--- kubejs/startup_scripts/cobblemon/registerCobblemonBlocks.js
+++ kubejs/startup_scripts/cobblemon/registerCobblemonBlocks.js
@@ -1,13 +1,35 @@
 console.info("[SOCIETY-S-COBBLEMON] registerCobblemonBlocks.js loaded");
 
+
+
 StartupEvents.registry("block", (e) => {
-  ['dawn', 'dusk', 'fire', 'ice', 'leaf', 'moon', 'shiny', 'sun', 'thunder', 'water'].forEach((stone) => {
-    e.create(`cobblemon:${stone}_stone_block`)
-      .soundType("amethyst")
-      .hardness(2)
-      .resistance(1.0)
-      .textureAll(`cobblemon:block/evolution/${stone}_stone_block`);
-  })
+  const cobblemonStone = (stone, lightLevel) => {
+    if (lightLevel && lightLevel > 0) {
+      e.create(`cobblemon:${stone}_stone_block`)
+        .soundType("amethyst")
+        .hardness(2)
+        .lightLevel(lightLevel)
+        .resistance(1.0)
+        .textureAll(`cobblemon:block/evolution/${stone}_stone_block`);
+    } else {
+
+      e.create(`cobblemon:${stone}_stone_block`)
+        .soundType("amethyst")
+        .hardness(2)
+        .resistance(1.0)
+        .textureAll(`cobblemon:block/evolution/${stone}_stone_block`);
+    }
+  }
+  cobblemonStone("fire", 0.9);
+  cobblemonStone("water");
+  cobblemonStone("thunder", 0.5);
+  cobblemonStone("leaf");
+  cobblemonStone("ice");
+  cobblemonStone("sun", 0.3);
+  cobblemonStone("moon");
+  cobblemonStone("shiny", 1);
+  cobblemonStone("dawn", 1);
+  cobblemonStone("dusk");
 
   e.create("sunlit_cobblemon:ominous_black_stake")
     .box(6, 0, 6, 10, 12, 10)
